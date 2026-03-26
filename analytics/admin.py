b2d25801f1a078e5eb17908f96a624a6d2ae71fc
radixpoint <chris.f.sifuentes@gmail.com>
2026-03-26 00:43:44 -0400

    Register models in admin for easier testing.

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Feedback
 
-# Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user_email', 'rating', 'is_resolved', 'created_at')
+    list_filter = ('rating', 'is_resolved')
