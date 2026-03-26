abb0cdd446aaf6064133629e79e0df1981c2aa8d
radixpoint <chris.f.sifuentes@gmail.com>
2026-03-26 00:29:40 -0400

    Add python-dotenv to support .env files.

--- django_drf/asgi.py
+++ django_drf/asgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_drf.settings')
 
 application = get_asgi_application()
